e4ccf6abf2036d56fa24f1084660a236a80d961d
MarinaGamal <32170558+MarinaGamal@users.noreply.github.com>
2023-06-23 15:32:48 +0200

    API Testing and updates in UI

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-"# QA-Task" 
+Testing My Store Website using NightwatchJS
+
+This README provides instructions on how to test the My Store website using NightwatchJS and to test mocha-auth-user API routes using supertest.
+ 
+NightwatchJS is a browser automation framework that allows you to write automated tests in JavaScript.
+Supertest is SuperTest is a library for testing HTTP servers in Node.js. It provides a high-level API for making HTTP requests and asserting the responses, allowing you to easily test your web applications and APIs.
+
+
+The reports and bug reports and documentation for both the UI and API parts are avaialable inside output folder in the root directory.
+
+For the first folder (UI):
+We are testing the UI for MyStore WebPage, specifically the ContactUs form and the searching for a dress function.
+
+Prerequisites for running the project:
+1.Node.js to be installed.
+2.NPM (Node Package Manager) to be installed.
+3.NightwatchJS to be installed.
+
+Getting Started (Windows Version):
+1.Clone  or download the repository 
+2.Navigate to UI/NightWatch folder in the project directory.
+3.Open the Command Line
+5.Run npm init nightwatch
+6.Choose the used browser, testing type and so on, you can find the details in the guide here: https://nightwatchjs.org/guide/quickstarts/create-and-run-a-nightwatch-test.html
+4.Run the following command for testing ContactUs form npx nightwatch test/MyStore/ContactUs.js
+5.The HTML report with the test cases output will be created in test-output/night-watch-html-report
+6.Run the following command for testing SearchDress function npx nightwatch test/MyStore/SearchDress.js
+7.The HTML report with the test cases output will be created in test-output/night-watch-html-report
+
+For the second folder (API):
+We are testing the mock-user-auth APIs using supertest and mocha as a test runner
+
+Prerequisites for running the project:
+1.SuperTest to be installed.
+2.mock-user-auth to be installed.
+3.Mocha test runner to be installed.
+
+Getting Started (Windows Version):
+1.Clone  or download the repository
+2.Navigate to API/MockUser folder in the project directory.
+3.Open the Command Line
+4.Run npm i --save mock-user-auth
+5.Create script in package.json:
+
+ {
+   "script": {
+     "dev": "nodemon ./node_modules/mock-user-auth/bin/www.js"
+   }
+ }
+6.npm run dev
+
+In a new command line while keepint the other one running run the following commands 
+7.npm install supertest --save-dev
+8.npm install --save-dev mocha
+9.npx mocha api.test.js
+
+To create HTML report I used mochawesome
+10.npm install --save-dev mochawesome
+11.Add this under scripts in the package.json file: "test": "mocha --reporter mochawesome api.test.js",
+12.Run npm test
+13.An HTML report with test cases will be created inside mochawesome-report
+
+
--- UI/NightWatch/test/MyStore/ContactUs.js
+++ UI/NightWatch/test/MyStore/ContactUs.js
@@ -9,8 +9,8 @@ describe('Contact Us Test', function () {
       .assert.visible('form.contact-form-box')
   });
   
-  //Test without all required fields
-  it('Without all required fields', function (browser) {
+  //Submit without all required fields
+  it('Submit with all blank', function (browser) {
 	browser.click('select[id="id_contact"] option[value="0"]');
 	//browser.verify.visible("#desc_contact0");
     browser.setValue('input#email', '');
@@ -19,12 +19,12 @@ describe('Contact Us Test', function () {
     browser.waitForElementVisible('.alert.alert-danger', 5000);
 	browser.waitForElementNotPresent ('.alert.alert-success',5000);
 	//browser.expect.element('.alert.alert-success').to.have.attribute('style').which.contains('display: none');
-    browser.verify.containsText('.alert.alert-danger', 'Subject Heading, Email and Message cannot be blank.');
+    browser.verify.containsText('.alert.alert-danger', 'Please select a subject from the list provided.');
 	
   });
   
-  // Test with Entering Subject Heading only
-  it('Entering Subject Heading only', function (browser) {
+  // Test with Entering Subject Heading only (Customer Service Option)
+  it('Entering Subject Heading only Customer Service', function (browser) {
     browser.click('select[id="id_contact"] option[value="2"]');
 	browser.verify.visible("#desc_contact2");
     browser.setValue('input#email', '');
@@ -32,15 +32,26 @@ describe('Contact Us Test', function () {
     browser.click('button#submitMessage');
     browser.waitForElementVisible('.alert.alert-danger', 5000);
 	browser.waitForElementNotPresent ('.alert.alert-success',5000);
-    browser.verify.containsText('.alert.alert-danger', 'Email and Message cannot be blank.');
+    browser.verify.containsText('.alert.alert-danger', 'Please enter email address');
+  });
+    // Test with Entering Subject Heading only (Webmaster Option)
+    it('Entering Subject Heading only Webmaster', function (browser) {
+    browser.click('select[id="id_contact"] option[value="1"]');
+	browser.verify.visible("#desc_contact1");
+    browser.setValue('input#email', '');
+    browser.setValue('textarea#message', '');
+    browser.click('button#submitMessage');
+    browser.waitForElementVisible('.alert.alert-danger', 5000);
+	browser.waitForElementNotPresent ('.alert.alert-success',5000);
+    browser.verify.containsText('.alert.alert-danger', 'Please enter email address');
   });
   
    // Test with Entering Invalid Email only
 	it('Entering Invalid Email only', function (browser) {
     browser.click('select[id="id_contact"] option[value="0"]');
     browser.setValue('input#email', 'test');
-    browser.setValue('textarea#message', ' ');
-	browser.pause(2000);
+    browser.setValue('textarea#message', '');
+	//browser.pause(2000);
 	//browser.expect.element('input#email[data-validate="isEmail"] + i.icon-invalid').to.be.visible;
     browser.click('button#submitMessage');
     browser.waitForElementVisible('.alert.alert-danger', 5000);
@@ -57,14 +68,11 @@ describe('Contact Us Test', function () {
     browser.click('button#submitMessage');
     browser.waitForElementVisible('.alert.alert-danger', 5000);
 	browser.waitForElementNotPresent ('.alert.alert-success',5000);
-    browser.verify.containsText('.alert.alert-danger', 'Subject Heading and Message cannot be blank');
+    browser.verify.containsText('.alert.alert-danger', 'Please select a subject from the list provided.');
 	});
 	
 	
    
-	
-	
-	
     // Test with Entering Message only
 	it('Entering Message only', function (browser) {
 
@@ -74,7 +82,7 @@ describe('Contact Us Test', function () {
     browser.click('button#submitMessage');
     browser.waitForElementVisible('.alert.alert-danger', 5000);
 	browser.waitForElementNotPresent ('.alert.alert-success',5000);
-    browser.verify.containsText('.alert.alert-danger', 'Email and Subject Heading cannot be blank.');
+    browser.verify.containsText('.alert.alert-danger', 'Please select a subject from the list provided.');
 	
 	});
 	
@@ -88,11 +96,11 @@ describe('Contact Us Test', function () {
     browser.click('button#submitMessage');
 	browser.waitForElementVisible('.alert.alert-danger', 5000);
 	browser.waitForElementNotPresent ('.alert.alert-success',5000);
-    browser.verify.containsText('.alert.alert-danger', 'Message cannot be blank.');
+    browser.verify.containsText('.alert.alert-danger', 'The message cannot be blank.');
 	});
 	
 	//Test with entering Heading and wrong email and a blank message 
-	it('Entering Subject Heading, wrong email and blank address', function (browser) {
+	it('Entering Subject Heading, wrong email and blank message', function (browser) {
 
 	browser.click('select[id="id_contact"] option[value="1"]');
 	browser.verify.visible("#desc_contact1");
@@ -101,7 +109,7 @@ describe('Contact Us Test', function () {
     browser.click('button#submitMessage');
 	browser.waitForElementVisible('.alert.alert-danger', 5000);
 	browser.waitForElementNotPresent ('.alert.alert-success',5000);
-    browser.verify.containsText('.alert.alert-danger', 'Please enter a valid email address and message.');
+    browser.verify.containsText('.alert.alert-danger', 'Invalid email address.');
 	});
 	
 	
@@ -126,7 +134,7 @@ describe('Contact Us Test', function () {
     browser.click('button#submitMessage');
 	browser.waitForElementVisible('.alert.alert-danger', 5000);
 	browser.waitForElementNotPresent ('.alert.alert-success',5000);
-    browser.verify.containsText('.alert.alert-danger', 'Email address cannot be blank.');
+    browser.verify.containsText('.alert.alert-danger', 'Please enter email address.');
 	});
 	
 	//Test with entering no Heading, valid email and a message 
@@ -138,71 +146,14 @@ describe('Contact Us Test', function () {
     browser.click('button#submitMessage');
 	browser.waitForElementVisible('.alert.alert-danger', 5000);
 	browser.waitForElementNotPresent ('.alert.alert-success',5000);
-    browser.verify.containsText('.alert.alert-danger', 'Subject Heading cannot be blank.');
+    browser.verify.containsText('.alert.alert-danger', 'Please select a subject from the list provided.');
 	
 	});
 	
-	//Test with entering all required fiels correctly  
-	it('Entering all required fields', function (browser) {
-
-	browser.click('select[id="id_contact"] option[value="2"]');
-	browser.verify.visible("#desc_contact2");
-    browser.setValue('input#email', 'test@test.com');
-    browser.setValue('textarea#message', 'Great!');
-    browser.click('button#submitMessage');
-	browser.waitForElementVisible('.alert.alert-success', 5000);
-	browser.waitForElementNotPresent ('.alert.alert-danger',5000);
-    browser.verify.containsText('.alert.alert-success', 'Your message has been successfully sent to our team.');
-	});
-/* 	
-	browser.pause(2000);
-	browser.url('http://automationpractice.multiformis.com/index.php?controller=contact');
-	//all fields
-	browser.click('select[id="id_contact"] option[value="1"]');
-    browser.setValue('input#email', 'test@test.com');
-    browser.setValue('textarea#message', 'Great!');
-    browser.click('button#submitMessage');
-	browser.waitForElementVisible('.alert.alert-success', 5000);
-    browser.verify.containsText('.alert.alert-success', 'Your message has been successfully sent to our team.');
-	
-	browser.pause(2000);
-	browser.url('http://automationpractice.multiformis.com/index.php?controller=contact');
-	
-	browser.pause(2000);
-	browser.back();
-	//all fields
-	browser.click('select[id="id_contact"] option[value="1"]');
-    browser.setValue('input#email', 'test@test.com');
-    browser.setValue('textarea#message', 'Great!');
-    browser.click('button#submitMessage');
-	browser.waitForElementVisible('.alert.alert-success', 5000); */
-    //browser.verify.containsText('.alert.alert-success', 'Your message has been successfully sent to our team.');
-	
-	//Test with Entering spaces only in the message field
-	it('Entering Spaces in the message field', function (browser) {
-
-	browser.pause(2000);
-	browser.url('http://automationpractice.multiformis.com/index.php?controller=contact');
-
-	
-	browser.click('select[id="id_contact"] option[value="1"]');
-	browser.verify.visible("#desc_contact1");
-    browser.setValue('input#email', 'test@test.com');
-    browser.setValue('textarea#message', '   ');
-    browser.click('button#submitMessage');
-	//browser.waitForElementVisible('.alert.alert-danger', 5000);   //stopped here is this right?
-	browser.verify.visible('.alert.alert-danger');
-	browser.waitForElementNotPresent ('.alert.alert-success',5000);
-    browser.verify.containsText('.alert.alert-danger', 'Message cannot be blank.');
-	
-	browser.pause(2000);
-	browser.url('http://automationpractice.multiformis.com/index.php?controller=contact');
-	
-  });
-  
-  
-  //Testing File Upload
+	//Testing File Upload
    it('Testing file upload', function (browser) {
+		browser.pause(2000);
+		browser.url('http://automationpractice.multiformis.com/index.php?controller=contact');
         const filePath = '../../upload/WebQAEngineer2023-Task.docx';
         const absoluteFilePath = require('path').resolve(__dirname, filePath);
         
@@ -243,5 +194,69 @@ describe('Contact Us Test', function () {
         }); 
 	 });
 
+	
+	//Test with entering all required fields correctly  
+	it('Entering all required fields', function (browser) {
+
+	browser.click('select[id="id_contact"] option[value="2"]');
+	browser.verify.visible("#desc_contact2");
+    browser.setValue('input#email', 'test@test.com');
+    browser.setValue('textarea#message', 'Great!');
+    browser.click('button#submitMessage');
+	browser.waitForElementVisible('.alert.alert-success', 5000);
+	//browser.waitForElementNotPresent ('.alert.alert-danger',5000);
+    browser.verify.containsText('.alert.alert-success', 'Your message has been successfully sent to our team.');
+	
+	browser.pause(2000);
+	browser.url('http://automationpractice.multiformis.com/index.php?controller=contact');
+	});
+/* 	
+	browser.pause(2000);
+	browser.url('http://automationpractice.multiformis.com/index.php?controller=contact');
+	//all fields
+	browser.click('select[id="id_contact"] option[value="1"]');
+    browser.setValue('input#email', 'test@test.com');
+    browser.setValue('textarea#message', 'Great!');
+    browser.click('button#submitMessage');
+	browser.waitForElementVisible('.alert.alert-success', 5000);
+    browser.verify.containsText('.alert.alert-success', 'Your message has been successfully sent to our team.');
+	
+	browser.pause(2000);
+	browser.url('http://automationpractice.multiformis.com/index.php?controller=contact');
+	
+	browser.pause(2000);
+	browser.back();
+	//all fields
+	browser.click('select[id="id_contact"] option[value="1"]');
+    browser.setValue('input#email', 'test@test.com');
+    browser.setValue('textarea#message', 'Great!');
+    browser.click('button#submitMessage');
+	browser.waitForElementVisible('.alert.alert-success', 5000); */
+    //browser.verify.containsText('.alert.alert-success', 'Your message has been successfully sent to our team.');
+	
+	
+	
+	//Test with Entering spaces only in the message field
+	it('Entering Spaces in the message field', function (browser) {
+
+	browser.pause(2000);
+	browser.url('http://automationpractice.multiformis.com/index.php?controller=contact');
+
+	
+	browser.click('select[id="id_contact"] option[value="1"]');
+	browser.verify.visible("#desc_contact1");
+    browser.setValue('input#email', 'test@test.com');
+    browser.setValue('textarea#message', '   ');
+    browser.click('button#submitMessage');
+	browser.verify.visible('.alert.alert-danger');
+    browser.verify.containsText('.alert.alert-danger', 'The message cannot be blank.');
+	
+	browser.pause(2000);
+	browser.url('http://automationpractice.multiformis.com/index.php?controller=contact');
+	
+  });
+  
+  
+  
   after(browser => browser.end());
 });
